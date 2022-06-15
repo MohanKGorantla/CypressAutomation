53e83c2916e26b0be052a07d08770912be7b115f
Mohan Gorantla <mohankrishna.gorantla@gmail.com>
2022-06-15 17:33:33 +1000

    Update 2-Sample.spec.cy.js
    Added aliasing, parent-child chaining and each methods

--- cypress/e2e/3-Practice/2-Sample.spec.cy.js
+++ cypress/e2e/3-Practice/2-Sample.spec.cy.js
@@ -5,5 +5,39 @@ describe('empty spec', function() {
       cy.get('input[type="search"]')
         .type('ca')
       cy.get('.product:visible').should('have.length',4)
+      // parent child chaining
+      cy.get('.products').find('.product').should('have.length',4)
+      // To iterate through array of elements and selecting the item based on condition
+      // Aliasing using as
+      cy.get('.products').as('productLocator')
+      cy.get('@productLocator').find('.product').each(($e1, index, $item) => {
+        const vegtext= $e1.find('h4.product-name').text()
+        if(vegtext.includes('Cashews'))
+        {
+          // Performing click on find method was depcreated and henxe we need to use cy.wrp to resolve the promise
+          cy.wrap($e1).find('button').click()
+        }
+
+      })
+      // Using jquery methods in cypress, we need resolve the promise before using jquery method(text())
+      cy.get('.brand').then(($el) => {
+        const logo = $el.text()
+        cy.log(logo)
+
+      })
+
+      // Using jquery methods in cypress, we need resolve the promise before using jquery method(text())
+      cy.get('.brand').then(function(logoelement){
+        cy.log(logoelement.text())
+
+      })
+
+      // Verifying the logo text
+      cy.get('.brand').should('have.text','GREENKART' )
+
+
+
+
+
     })
   })
